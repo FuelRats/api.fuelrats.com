13d79c6edd669945233dfe04ec5d39e31f357235
Trezy <tre@trezy.com>
2015-12-20 11:32:02 -0600

    Adding login files

---
+++ api/controllers/login.js
@@ -0,0 +1,15 @@
+var path
+
+
+
+
+
+path = require( 'path' )
+
+
+
+
+
+exports.get = function ( request, response ) {
+  response.sendFile( path.join( __dirname + '/../templates/login.html' ) )
+}
