13dcd6324ae65e8670f3a03e690bef273c4f583e
Trezy <tre@trezy.com>
2015-12-20 10:27:56 -0600

    Housekeeping

--- api/controllers/rescue.js
+++ api/controllers/rescue.js
@@ -1,11 +1,11 @@
-var Rescue, rescue, save;
+var Rescue, rescue, save
 
 
 
 
 
-Rescue = require( '../models/rescue' );
-ErrorModels = require( '../errors' );
+Rescue = require( '../models/rescue' )
+ErrorModels = require( '../errors' )
 
 
 
@@ -14,51 +14,51 @@ ErrorModels = require( '../errors' );
 // GET
 // =============================================================================
 exports.get = function ( request, response ) {
-  var id, responseModel;
+  var id, responseModel
 
   responseModel = {
     links: {
       self: request.originalUrl
     }
-  };
+  }
 
   if ( id = request.params.id ) {
     Rescue.findById( id, function ( error, rescue ) {
-      var status;
+      var status
 
       if ( error ) {
-        responseModel.errors = [];
-        responseModel.errors.push( error );
-        status = 400;
+        responseModel.errors = []
+        responseModel.errors.push( error )
+        status = 400
 
       } else {
-        responseModel.data = rescue;
-        status = 200;
+        responseModel.data = rescue
+        status = 200
       }
 
-      response.status( status );
-      response.json( responseModel );
-    });
+      response.status( status )
+      response.json( responseModel )
+    })
 
   } else {
     Rescue.find( request.body, function ( error, rescues ) {
-      var status;
+      var status
 
       if ( error ) {
-        responseModel.errors = [];
-        responseModel.errors.push( error );
-        status = 400;
+        responseModel.errors = []
+        responseModel.errors.push( error )
+        status = 400
 
       } else {
-        responseModel.data = rescues;
-        status = 200;
+        responseModel.data = rescues
+        status = 200
       }
 
-      response.status( status );
-      response.json( responseModel );
-    });
+      response.status( status )
+      response.json( responseModel )
+    })
   }
-};
+}
 
 
 
@@ -67,50 +67,50 @@ exports.get = function ( request, response ) {
 // POST
 // =============================================================================
 exports.post = function ( request, response ) {
-  var responseModel;
+  var responseModel
 
   responseModel = {
     links: {
       self: request.originalUrl
     }
-  };
+  }
 
   Rescue.create( request.body, function ( error, rescue ) {
-    var errors, errorTypes, status;
+    var errors, errorTypes, status
 
     if ( error ) {
-      errorTypes = Object.keys( error.errors );
-      responseModel.errors = [];
+      errorTypes = Object.keys( error.errors )
+      responseModel.errors = []
 
       for ( var i = 0; i < errorTypes.length; i++ ) {
-        var error, errorModel, errorType;
+        var error, errorModel, errorType
 
-        errorType = errorTypes[i];
-        error = error.errors[errorType].properties;
+        errorType = errorTypes[i]
+        error = error.errors[errorType].properties
 
         if ( error.type === 'required' ) {
-          errorModel = ErrorModels['missing_required_field'];
+          errorModel = ErrorModels['missing_required_field']
         }
 
-        errorModel.detail = 'You\'re missing the required field: ' + error.path;
+        errorModel.detail = 'You\'re missing the required field: ' + error.path
 
-        responseModel.errors.push( errorModel );
+        responseModel.errors.push( errorModel )
       }
 
-      console.log( 'failed', error );
-      status = 400;
+      console.log( 'failed', error )
+      status = 400
 
     } else {
-      responseModel.data = rescue;
-      status = 201;
+      responseModel.data = rescue
+      status = 201
     }
 
-    response.status( status );
-    response.json( responseModel );
-  });
+    response.status( status )
+    response.json( responseModel )
+  })
 
-  return rescue;
-};
+  return rescue
+}
 
 
 
@@ -119,74 +119,74 @@ exports.post = function ( request, response ) {
 // PUT
 // =============================================================================
 exports.put = function ( request, response ) {
-  var responseModel, status;
+  var responseModel, status
 
   responseModel = {
     links: {
       self: request.originalUrl
     }
-  };
+  }
 
   if ( id = request.params.id ) {
     Rescue.findById( id, function ( error, rescue ) {
       if ( error ) {
-        responseModel.errors = responseModel.errors || [];
-        responseModel.errors.push( error );
-        response.status( 400 );
-        response.json( responseModel );
-        return;
+        responseModel.errors = responseModel.errors || []
+        responseModel.errors.push( error )
+        response.status( 400 )
+        response.json( responseModel )
+        return
 
       } else if ( !rescue ) {
-        response.status( 404 ).send();
-        return;
+        response.status( 404 ).send()
+        return
       }
 
       for ( var key in request.body ) {
         rescue[key] = request.body[key]
       }
 
-      rescue.increment();
+      rescue.increment()
       rescue.save( function ( error, rescue ) {
-        var errors, errorTypes, status;
+        var errors, errorTypes, status
 
         if ( error ) {
 
-          errorTypes = Object.keys( error.errors );
-          responseModel.errors = [];
+          errorTypes = Object.keys( error.errors )
+          responseModel.errors = []
 
           for ( var i = 0; i < errorTypes.length; i++ ) {
-            var error, errorModel, errorType;
+            var error, errorModel, errorType
 
-            errorType = errorTypes[i];
-            error = error.errors[errorType].properties;
+            errorType = errorTypes[i]
+            error = error.errors[errorType].properties
 
             if ( error.type === 'required' ) {
-              errorModel = ErrorModels['missing_required_field'];
+              errorModel = ErrorModels['missing_required_field']
             }
 
-            errorModel.detail = 'You\'re missing the required field: ' + error.path;
+            errorModel.detail = 'You\'re missing the required field: ' + error.path
 
-            responseModel.errors.push( errorModel );
+            responseModel.errors.push( errorModel )
           }
 
-          status = 400;
+          status = 400
 
         } else {
-          status = 200;
-          responseModel.data = rescue;
+          status = 200
+          responseModel.data = rescue
         }
 
-        response.status( status );
-        response.json( responseModel );
-      });
-    });
+        response.status( status )
+        response.json( responseModel )
+      })
+    })
   } else {
-    response.status( 400 );
-    response.send();
+    response.status( 400 )
+    response.send()
   }
 
-  return rescue;
-};
+  return rescue
+}
 
 
 
@@ -195,27 +195,27 @@ exports.put = function ( request, response ) {
 // SEARCH
 // =============================================================================
 exports.search = function ( request, response ) {
-  var query, responseModel, scoring;
+  var query, responseModel, scoring
 
   responseModel = {
     links: {
       self: request.originalUrl
     }
-  };
-  scoring = {};
+  }
+  scoring = {}
 
   if ( request.params.query ) {
     query = {
       $text: {
         $search: request.params.query
       }
-    };
+    }
 
     scoring.score = {
       $meta: 'textScore'
-    };
+    }
   } else {
-    query = request.body;
+    query = request.body
   }
 
   Rescue
@@ -224,15 +224,15 @@ exports.search = function ( request, response ) {
   .limit( 10 )
   .exec( function ( error, rescues ) {
     if ( error ) {
-      responseModel.errors = [];
-      responseModel.errors.push( error );
-      status = 400;
+      responseModel.errors = []
+      responseModel.errors.push( error )
+      status = 400
     } else {
-      responseModel.data = rescues;
-      status = 200;
+      responseModel.data = rescues
+      status = 200
     }
 
-    response.status( status );
-    response.json( responseModel );
-  });
-};
+    response.status( status )
+    response.json( responseModel )
+  })
+}
