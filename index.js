05543c4cc50d8403ec0d3ba768f0c49a5d546597
Trezy <tre@trezy.com>
2016-01-21 15:05:46 -0600

    Confirm the websocket server is working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var _, app, badge, bodyParser, config, cors, docket, docs, express, expressSession, fs, http, httpServer, io, LocalStrategy, logger, login, mongoose, notAllowed, options, passport, path, port, Rat, rat, register, Rescue, rescue, router, socket;
+var _, app, badge, bodyParser, config, cookieParser, cors, docket, docs, express, expressSession, fs, http, httpServer, io, LocalStrategy, logger, login, mongoose, notAllowed, options, passport, path, port, Rat, rat, register, Rescue, rescue, router, socket, ws
 
 
 
@@ -8,20 +8,21 @@ var _, app, badge, bodyParser, config, cors, docket, docs, express, expressSessi
 // =============================================================================
 
 // Import libraries
-_ = require( 'underscore' );
-bodyParser = require( 'body-parser' );
-cors = require( 'cors' );
-// docket = require( './docket.js' );
-docs = require( 'express-mongoose-docs' );
-express = require( 'express' );
-expressSession = require( 'express-session' );
-fs = require( 'fs' );
-http = require( 'http' );
-mongoose = require( 'mongoose' );
-passport = require( 'passport' );
-path = require( 'path' );
-io = require( 'socket.io' );
-LocalStrategy = require( 'passport-local' ).Strategy;
+_ = require( 'underscore' )
+bodyParser = require( 'body-parser' )
+cors = require( 'cors' )
+cookieParser = require( 'cookie-parser' )
+// docket = require( './docket.js' )
+docs = require( 'express-mongoose-docs' )
+express = require( 'express' )
+expressSession = require( 'express-session' )
+fs = require( 'fs' )
+http = require( 'http' )
+mongoose = require( 'mongoose' )
+passport = require( 'passport' )
+path = require( 'path' )
+LocalStrategy = require( 'passport-local' ).Strategy
+ws = require( 'ws' ).Server
 
 // Import config
 if ( fs.existsSync( './config.json' ) ) {
@@ -31,24 +32,24 @@ if ( fs.existsSync( './config.json' ) ) {
 }
 
 // Import models
-Rat = require( './api/models/rat' );
-Rescue = require( './api/models/rescue' );
-User = require( './api/models/user' );
+Rat = require( './api/models/rat' )
+Rescue = require( './api/models/rescue' )
+User = require( './api/models/user' )
 
 // Import controllers
-badge = require( './api/controllers/badge' );
-login = require( './api/controllers/login' );
-rat = require( './api/controllers/rat' );
-register = require( './api/controllers/register' );
-rescue = require( './api/controllers/rescue' );
+badge = require( './api/controllers/badge' )
+login = require( './api/controllers/login' )
+rat = require( './api/controllers/rat' )
+register = require( './api/controllers/register' )
+rescue = require( './api/controllers/rescue' )
 
 // Connect to MongoDB
-mongoose.connect( 'mongodb://localhost/fuelrats' );
+mongoose.connect( 'mongodb://localhost/fuelrats' )
 
 options = {
   logging: true,
   test: false
-};
+}
 
 
 
@@ -59,9 +60,9 @@ options = {
 
 // Function for disallowed methods
 notAllowed = function notAllowed ( request, response ) {
-  response.status( 405 );
-  response.send();
-};
+  response.status( 405 )
+  response.send()
+}
 
 
 
@@ -72,18 +73,18 @@ notAllowed = function notAllowed ( request, response ) {
 
 if ( process.argv ) {
   for ( var i = 0; i < process.argv.length; i++ ) {
-    var arg;
+    var arg
 
-    arg = process.argv[i];
+    arg = process.argv[i]
 
     switch ( arg ) {
       case '--no-log':
-        options.logging = false;
-        break;
+        options.logging = false
+        break
 
       case '--test':
-        options.test = true;
-        break;
+        options.test = true
+        break
     }
   }
 }
@@ -95,55 +96,56 @@ if ( process.argv ) {
 // MIDDLEWARE
 // =============================================================================
 
-app = express();
-app.use( cors() );
-app.use( bodyParser.urlencoded( { extended: true } ) );
-app.use( bodyParser.json() );
+app = express()
+app.use( cors() )
+app.use( bodyParser.urlencoded( { extended: true } ) )
+app.use( bodyParser.json() )
+app.use( cookieParser() )
 app.use( expressSession({
   secret: config.secretSauce,
   resave: false,
   saveUninitialized: false
-}));
-app.use( passport.initialize() );
-app.use( passport.session() );
+}))
+app.use( passport.initialize() )
+app.use( passport.session() )
 
-app.set( 'json spaces', 2 );
-app.set( 'x-powered-by', false );
+app.set( 'json spaces', 2 )
+app.set( 'x-powered-by', false )
 
-httpServer = http.Server( app );
+httpServer = http.Server( app )
 
-port = process.env.PORT || config.port;
+port = process.env.PORT || config.port
 
-passport.use( User.createStrategy() );
-passport.serializeUser( User.serializeUser() );
-passport.deserializeUser( User.deserializeUser() );
+passport.use( User.createStrategy() )
+passport.serializeUser( User.serializeUser() )
+passport.deserializeUser( User.deserializeUser() )
 
 app.use( expressSession({
   secret: 'foobarbazdiddlydingdongsdf]08st0agf/b',
   resave: false,
   saveUninitialized: false
-}));
-app.use( passport.initialize() );
-app.use( passport.session() );
+}))
+app.use( passport.initialize() )
+app.use( passport.session() )
 
-docs( app, mongoose );
-// docket( app, mongoose );
+docs( app, mongoose )
+// docket( app, mongoose )
 
 // Combine query parameters with the request body, prioritizing the body
 app.use( function ( request, response, next ) {
-  request.body = _.extend( request.query, request.body );
-  next();
-});
+  request.body = _.extend( request.query, request.body )
+  next()
+})
 
 // Add logging
 if ( options.logging || options.test ) {
   app.use( function ( request, response, next ) {
-    console.log( '' );
-    console.log( 'ENDPOINT:', request.originalUrl );
-    console.log( 'METHOD:', request.method );
-    console.log( 'DATA:', request.body );
-    next();
-  });
+    console.log( '' )
+    console.log( 'ENDPOINT:', request.originalUrl )
+    console.log( 'METHOD:', request.method )
+    console.log( 'DATA:', request.body )
+    next()
+  })
 }
 
 
@@ -154,7 +156,7 @@ if ( options.logging || options.test ) {
 // =============================================================================
 
 // Create router
-router = express.Router();
+router = express.Router()
 
 
 
@@ -163,44 +165,45 @@ router = express.Router();
 // ROUTES
 // =============================================================================
 
-app.get( '/badge/:rat', badge.get );
+app.get( '/badge/:rat', badge.get )
 
-app.get( '/register', register.get );
-app.post( '/register', register.post );
+app.get( '/register', register.get )
+app.post( '/register', register.post )
 
-app.get( '/login', login.get );
-app.post( '/login', passport.authenticate( 'local' ), login.post );
+app.get( '/login', login.get )
+app.post( '/login', passport.authenticate( 'local' ), login.post )
 
-router.get( '/rats/:id', rat.get );
-router.post( '/rats/:id', rat.post );
-router.put( '/rats/:id', rat.put );
-router.delete( '/rats/:id', notAllowed );
+router.get( '/rats/:id', rat.get )
+router.post( '/rats/:id', rat.post )
+router.put( '/rats/:id', rat.put )
+router.delete( '/rats/:id', notAllowed )
 
-router.get( '/rats', rat.get );
-router.post( '/rats', rat.post );
-router.put( '/rescues', notAllowed );
-router.delete( '/rescues', notAllowed );
+router.get( '/rats', rat.get )
+router.post( '/rats', rat.post )
+router.put( '/rescues', notAllowed )
+router.delete( '/rescues', notAllowed )
 
-router.get( '/rescues/:id', rescue.get );
-router.post( '/rescues/:id', rescue.post );
-router.put( '/rescues/:id', rescue.put );
-router.delete( '/rescues/:id', notAllowed );
+router.get( '/rescues/:id', rescue.get )
+router.post( '/rescues/:id', rescue.post )
+router.put( '/rescues/:id', rescue.put )
+router.delete( '/rescues/:id', notAllowed )
 
-router.get( '/rescues', rescue.get );
-router.post( '/rescues', rescue.post );
-router.put( '/rescues', notAllowed );
-router.delete( '/rescues', notAllowed );
+router.get( '/rescues', rescue.get )
+router.post( '/rescues', rescue.post )
+router.put( '/rescues', notAllowed )
+router.delete( '/rescues', notAllowed )
 
-router.get( '/search/rescues', rescue.get );
+router.get( '/search/rescues', rescue.get )
 
-router.get( '/search/rats', rat.get );
+router.get( '/search/rats', rat.get )
 
 // Register routes
-app.use( '/', router );
-app.use( '/api', router );
-app.use( '/assets', express.static( __dirname + '/assets' ) );
-app.use( '/bower_components', express.static( __dirname + '/bower_components' ) );
-app.use( '/node_modules', express.static( __dirname + '/node_modules' ) );
+app.use( express.static( __dirname + '/static' ) )
+app.use( '/', router )
+app.use( '/api', router )
+app.use( '/assets', express.static( __dirname + '/assets' ) )
+app.use( '/bower_components', express.static( __dirname + '/bower_components' ) )
+app.use( '/node_modules', express.static( __dirname + '/node_modules' ) )
 
 
 
@@ -209,11 +212,13 @@ app.use( '/node_modules', express.static( __dirname + '/node_modules' ) );
 // SOCKET
 // =============================================================================
 
-socket = io( httpServer );
+socket = new ws({ server: httpServer })
 
 socket.on( 'connection', function ( socket ) {
-  console.log( 'a user connected' );
-});
+  socket.send( JSON.stringify({
+    data: 'Welcome! You\'ve connected to the Fuel Rats API server. Now GO AWAY.'
+  }))
+})
 
 
 
@@ -222,8 +227,8 @@ socket.on( 'connection', function ( socket ) {
 // START THE SERVER
 // =============================================================================
 
-module.exports = httpServer.listen( port );
+module.exports = httpServer.listen( port )
 
 if ( !module.parent ) {
-  console.log( 'Listening for requests on port ' + port + '...' );
+  console.log( 'Listening for requests on port ' + port + '...' )
 }
