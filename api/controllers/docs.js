7ea2cb5f4ff40d42a55b7f6827b84ab28671fb92
Trezy <tre@trezy.com>
2016-01-29 09:37:53 -0600

    Minor changes to the docs endpoint

--- api/controllers/docs.js
+++ api/controllers/docs.js
@@ -1,13 +1,11 @@
-var winston,
-winston = require( 'winston' )
-
-exports.get = function (request, response) {
-  response.render('docs', { layout: false }, function (err, html) {
-    if ( err ) {
-      winston.error(err)
-      response.send('Unable to find documentation. Please consult the included README')
-    } else {
-      response.send(html)
+exports.get = function ( request, response ) {
+  response.render( 'docs', {
+    layout: false
+  }, function ( error, html ) {
+    if ( error ) {
+      return response.send( 'Unable to find documentation. Please consult the included README' )
     }
+
+    response.send( html )
   })
 }
