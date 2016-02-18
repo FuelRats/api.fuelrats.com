fc1fd877f86e16bb86fd5c2449faa29686f5afe1
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-02-18 21:26:34 +0100

    Support rat queries in websocket

--- api/controllers/rat.js
+++ api/controllers/rat.js
@@ -17,86 +17,17 @@ ErrorModels = require( '../errors' )
 // GET
 // =============================================================================
 exports.get = function ( request, response, next ) {
-  var filter, query
+  exports.view( request.body ).then( function( res ) {
+    var data = res.data
+    var meta = res.meta
 
-  filter = {}
-  query = {}
-
-  filter.size = parseInt( request.body.limit ) || 25
-  delete request.body.limit
-
-  filter.from = parseInt( request.body.offset ) || 0
-  delete request.body.offset
-
-  for ( var key in request.body ) {
-    if ( key === 'q' ) {
-      query.query_string = {
-        query: request.body.q
-      }
-    } else {
-      if ( !query.bool ) {
-        query.bool = {
-          should: []
-        }
-      }
-
-      term = {}
-      term[key] = {
-        query: request.body[key],
-        fuzziness: 'auto'
-      }
-      query.bool.should.push( { match: term } )
-    }
-  }
-
-  if ( !Object.keys( query ).length ) {
-    query.match_all = {}
-  }
-
-  Rat.search( query, filter, function ( error, data ) {
-    var rescueFinds
-
-    rescueFinds = []
-
-    if ( error ) {
-      response.model.errors.push( error )
-      response.status( 400 )
-
-    } else {
-      response.model.meta = {
-        count: data.hits.hits.length,
-        limit: filter.size,
-        offset: filter.from,
-        total: data.hits.total
-      }
-      response.model.data = []
-
-      data.hits.hits.forEach( function ( rat, index, rats ) {
-        var ratToPopulate, rescueFind
-
-        rat._source._id = rat._id
-        rat._source.score = rat._score
-
-//        rescueFind = Rescue.find({
-//          platform: rat._source.platform,
-//          rats: rat._source.CMDRname
-//        })
-
-        response.model.data.push( rat._source )
-
-//        rescueFind.then( function ( rescues ) {
-//          rat._source.rescues = rescues
-//        })
-
-        rescueFinds.push( rescueFind )
-      })
-
-      response.status( 200 )
-    }
-
-//    Promise.all( rescueFinds )
-//    .then( next )
+    response.model.data = data
+    response.model.meta = meta
+    response.status = 400
     next()
+  }, function( error ) {
+    response.model.errors.push( error.error )
+    response.status( 400 )
   })
 }
 
@@ -134,7 +65,79 @@ exports.getById = function ( request, response, next ) {
 }
 
 
+exports.view = function ( query ) {
+  return new Promise(function(resolve, reject) {
+    var filter, dbQuery
 
+    filter = {}
+    dbQuery = {}
+
+    filter.size = parseInt( query.limit ) || 25
+    delete query.limit
+
+    filter.from = parseInt( query.offset ) || 0
+    delete query.offset
+
+    for ( var key in query ) {
+      if ( key === 'q' ) {
+        dbQuery.query_string = {
+          query: query.q
+        }
+      } else {
+        if ( !dbQuery.bool ) {
+          dbQuery.bool = {
+            should: []
+          }
+        }
+
+        term = {}
+        term[key] = {
+          query: query[key],
+          fuzziness: 'auto'
+        }
+        dbQuery.bool.should.push( { match: term } )
+      }
+    }
+
+    if ( !Object.keys( dbQuery ).length ) {
+      dbQuery.match_all = {}
+    }
+
+    Rat.search( dbQuery, filter, function ( error, dbData ) {
+      var rescueFinds
+
+      rescueFinds = []
+
+      if ( error ) {
+        var errorObj = ErrorModels.server_error
+        errorObj.detail = error
+        reject({error: errorObj, meta: {}})
+
+      } else {
+        var meta = {
+          count: dbData.hits.hits.length,
+          limit: filter.size,
+          offset: filter.from,
+          total: dbData.hits.total
+        }
+        var data = []
+
+        dbData.hits.hits.forEach( function ( rat, index, rats ) {
+          var ratToPopulate, rescueFind
+
+          rat._source._id = rat._id
+          rat._source.score = rat._score
+
+
+          data.push( rat._source )
+          rescueFinds.push( rescueFind )
+        })
+
+        resolve({ data: data, meta: meta})
+      }
+    })
+  })
+}
 
 
 // POST
