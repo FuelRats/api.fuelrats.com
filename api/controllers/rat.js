b671fc235e988e545154ba184f85eed8a80f9460
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-02-20 02:40:11 +0100

    Finish support for Rat endpoint in websocket

--- api/controllers/rat.js
+++ api/controllers/rat.js
@@ -1,259 +1,217 @@
-var _, ErrorModels, Rat, rat, Rescue, save, winston
+'use strict';
 
-
-
-
-
-_ = require( 'underscore' )
-winston = require( 'winston' )
-Rat = require( '../models/rat' )
-Rescue = require( '../models/rescue' )
-ErrorModels = require( '../errors' )
-
-
-
-
+let _ = require('underscore');
+let Rat = require('../models/rat');
+let ErrorModels = require('../errors');
 
 // GET
 // =============================================================================
-exports.get = function ( request, response, next ) {
-  exports.read( request.body ).then( function( res ) {
-    var data = res.data
-    var meta = res.meta
+exports.get = function(request, response, next) {
+    exports.read(request.body).then(function(res) {
+        let data = res.data;
+        let meta = res.meta;
 
-    response.model.data = data
-    response.model.meta = meta
-    response.status = 400
-    next()
-  }, function( error ) {
-    response.model.errors.push( error.error )
-    response.status( 400 )
-  })
-}
-
+        response.model.data = data;
+        response.model.meta = meta;
+        response.status = 400;
+        next();
+    }, function(error) {
+        response.model.errors.push(error.error);
+        response.status(400);
+    });
+};
 
 
 
 
 // GET (by ID)
 // =============================================================================
-exports.getById = function ( request, response, next ) {
-  var id
+exports.getById = function(request, response, next) {
+    response.model.meta.params = _.extend(response.model.meta.params, request.params);
 
-  response.model.meta.params = _.extend( response.model.meta.params, request.params )
-  console.log( response.model.meta.params )
+    let id = request.params.id;
 
-  id = request.params.id
+    Rat.findById(id).populate('rescues').exec(function(error, rat) {
+        if (error) {
+            response.model.errors.push(error);
+            response.status(400);
 
-  Rat
-  .findById( id )
-  .populate( 'rescues' )
-  .exec( function ( error, rat ) {
-    var status
-
-    if ( error ) {
-      response.model.errors.push( error )
-      response.status( 400 )
-
-    } else {
-      response.model.data = rat
-      response.status( 200 )
-    }
-
-    next()
-  })
-}
-
-
-exports.read = function ( query ) {
-  return new Promise(function(resolve, reject) {
-    var filter, dbQuery
-
-    filter = {}
-    dbQuery = {}
-
-    filter.size = parseInt( query.limit ) || 25
-    delete query.limit
-
-    filter.from = parseInt( query.offset ) || 0
-    delete query.offset
-
-    for ( var key in query ) {
-      if ( key === 'q' ) {
-        dbQuery.query_string = {
-          query: query.q
-        }
-      } else {
-        if ( !dbQuery.bool ) {
-          dbQuery.bool = {
-            should: []
-          }
+        } else {
+            response.model.data = rat;
+            response.status(200);
         }
 
-        term = {}
-        term[key] = {
-          query: query[key],
-          fuzziness: 'auto'
+        next();
+    });
+};
+
+
+exports.read = function(query) {
+    return new Promise(function(resolve, reject) {
+        let filter = {};
+        let dbQuery = {};
+
+        filter.size = parseInt(query.limit) || 25;
+        delete query.limit;
+
+        filter.from = parseInt(query.offset) || 0;
+        delete query.offset;
+
+        for (var key in query) {
+            if (key === 'q') {
+                dbQuery.query_string = {
+                    query: query.q
+                };
+            } else {
+                if (!dbQuery.bool) {
+                    dbQuery.bool = {
+                        should: []
+                    };
+                }
+
+                let term = {};
+                term[key] = {
+                    query: query[key],
+                    fuzziness: 'auto'
+                };
+                dbQuery.bool.should.push({
+                    match: term
+                });
+            }
         }
-        dbQuery.bool.should.push( { match: term } )
-      }
-    }
 
-    if ( !Object.keys( dbQuery ).length ) {
-      dbQuery.match_all = {}
-    }
-
-    Rat.search( dbQuery, filter, function ( error, dbData ) {
-      var rescueFinds
-
-      rescueFinds = []
-
-      if ( error ) {
-        var errorObj = ErrorModels.server_error
-        errorObj.detail = error
-        reject({error: errorObj, meta: {}})
-
-      } else {
-        var meta = {
-          count: dbData.hits.hits.length,
-          limit: filter.size,
-          offset: filter.from,
-          total: dbData.hits.total
+        if (!Object.keys(dbQuery).length) {
+            dbQuery.match_all = {};
         }
-        var data = []
 
-        dbData.hits.hits.forEach( function ( rat, index, rats ) {
-          var ratToPopulate, rescueFind
+        Rat.search(dbQuery, filter, function(error, dbData) {
+            if (error) {
+                let errorObj = ErrorModels.server_error;
+                errorObj.detail = error;
+                reject({
+                    error: errorObj,
+                    meta: {}
+                });
 
-          rat._source._id = rat._id
-          rat._source.score = rat._score
+            } else {
+                let meta = {
+                    count: dbData.hits.hits.length,
+                    limit: filter.size,
+                    offset: filter.from,
+                    total: dbData.hits.total
+                };
+                let data = [];
 
+                dbData.hits.hits.forEach(function(rat) {
+                    rat._source._id = rat._id;
+                    rat._source.score = rat._score;
 
-          data.push( rat._source )
-          rescueFinds.push( rescueFind )
-        })
+                    data.push(rat._source);
+                });
 
-        resolve({ data: data, meta: meta})
-      }
-    })
-  })
-}
+                resolve({
+                    data: data,
+                    meta: meta
+                });
+            }
+        });
+    });
+};
 
 
 // POST
 // =============================================================================
-exports.post = function ( request, response, next ) {
-  Rat.create( request.body, function ( error, rat ) {
-    var errors, errorTypes, status
-
-    if ( error ) {
-      errorTypes = Object.keys( error.errors )
-
-      for ( var i = 0; i < errorTypes.length; i++ ) {
-        var error, errorModel, errorType
-
-        errorType = errorTypes[i]
-        error = error.errors[errorType].properties
-
-        if ( error.type === 'required' ) {
-          errorModel = ErrorModels['missing_required_field']
-        }
-
-        errorModel.detail = 'You\'re missing the required field: ' + error.path
-
-        response.model.errors.push( errorModel )
-      }
-
-      winston.error( error )
-      response.status( 400 )
-
-    } else {
-      response.model.data = rat
-      response.status( 201 )
-    }
-
-    next()
-
-//    if ( referer = request.get( 'Referer' ) ) {
-//      response.redirect( '/login' )
-//
-//    } else {
-//      response.status( status )
-//      response.json( response.model )
-//    }
-  })
-}
+exports.post = function(request, response, next) {
+    exports.create( request.body ).then(function( res ) {
+        response.model.data = res.data;
+        response.status(201);
+        next();
+    }, function( error ) {
+        response.model.errors.push(error.error);
+        response.status(400);
+        next();
+    });
+};
 
 
+exports.create = function(query) {
+    return new Promise(function(resolve, reject) {
+        Rat.create(query, function(error, rat) {
+            if (error) {
+                let errorTypes = Object.keys(error.errors);
 
+                for (var i = 0; i < errorTypes.length; i++) {
+                    let errorType = errorTypes[i];
+                    error = error.errors[errorType].properties;
+
+                    if (error.type === 'required') {
+                        let errorModel = ErrorModels.missing_required_field;
+                        errorModel.detail = error.path;
+                        reject({ error: errorModel, meta: {} });
+                    } else {
+                        let errorModel = ErrorModels.server_error;
+                        errorModel.detail = error.path;
+                        reject({ error: errorModel, meta: {} });
+                    }
+                }
+            } else {
+                resolve({ data: rat, meta: {} });
+            }
+        });
+    });
+};
 
 
 // PUT
 // =============================================================================
-exports.put = function ( request, response, next ) {
-  var status
+exports.put = function(request, response, next) {
+    response.model.meta.params = _.extend(response.model.meta.params, request.params);
 
-  response.model.meta.params = _.extend( response.model.meta.params, request.params )
+    exports.update(request.body, null, request.params).then(function (data) {
+        response.model.data = data.data;
+        response.status(201);
+        next();
+    }, function (error) {
+        response.model.errors.push(error.error);
 
-  if ( id = request.params.id ) {
-    Rat.findById( id, function ( error, rat ) {
-      if ( error ) {
-        response.model.errors.push( error )
-        response.status( 400 )
+        var status = error.error.code || 400;
+        response.status(status);
+        next();
+    });
+};
 
-        next()
+exports.update = function (data, client, query) {
+    return new Promise(function (resolve, reject) {
+        if (query.id) {
+            Rat.findById(query.id, function(error, rat) {
+                if (error) {
+                    let errorModel = ErrorModels.server_error;
+                    errorModel.detail = error;
+                    reject({ error: errorModel, meta: {}});
+                } else if (!rat) {
+                    let errorModel = ErrorModels.not_found;
+                    errorModel.detail = query.id;
+                    reject({ error: errorModel, meta: {}});
+                } else {
+                    for (var key in data) {
+                        if (key === 'client') {
+                            _.extend(rat.client, data);
+                        } else {
+                            rat[key] = data[key];
+                        }
+                    }
 
-      } else if ( !rat ) {
-        response.model.errors.push( ErrorModels.not_found )
-        response.status( 404 )
-
-        next()
-
-      } else {
-        for ( var key in request.body ) {
-          if ( key === 'client' ) {
-            _.extend( rat.client, request.body[key] )
-          } else {
-            rat[key] = request.body[key]
-          }
+                    rat.save(function(error, rat) {
+                        if (error) {
+                            let errorModel = ErrorModels.server_error;
+                            errorModel.detail = error;
+                            reject({ error: errorModel, meta: {}});
+                        } else {
+                            resolve({ data: rat, meta: {} });
+                        }
+                    });
+                }
+            });
         }
-
-        rat.save( function ( error, rat ) {
-          var errors, errorTypes, status
-
-          if ( error ) {
-            errorTypes = Object.keys( error.errors )
-
-            for ( var i = 0; i < errorTypes.length; i++ ) {
-              var error, errorModel, errorType
-
-              errorType = errorTypes[i]
-              error = error.errors[errorType].properties
-
-              if ( error.type === 'required' ) {
-                errorModel = ErrorModels['missing_required_field']
-              }
-
-              errorModel.detail = 'You\'re missing the required field: ' + error.path
-
-              response.model.errors.push( errorModel )
-            }
-
-            status = 400
-
-          } else {
-            status = 200
-            response.model.data = rat
-          }
-
-          next()
-        })
-      }
-    })
-  } else {
-    response.model.errors.push( ErrorModels.missing_required_field )
-    response.status( 400 )
-
-    next()
-  }
-}
+    });
+};
