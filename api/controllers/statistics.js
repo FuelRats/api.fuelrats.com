17d30e15efba7cc2d759f95bbba9806542de0abb
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-02-23 15:37:03 +0100

    Add leaderboard main graph generation (unfinished)

--- api/controllers/statistics.js
+++ api/controllers/statistics.js
@@ -1,39 +1,71 @@
 'use strict'
 
 let Rescue = require('../models/rescue')
-let mongoose = require('mongoose')
-let winston = require('winston')
 
 exports.get = function (request, response, next) {
-  response.model.data = {}
-  response.status(200)
-  getOverallRescueCount()
-  next()
+  getOverallRescueCount().then(function (values) {
+    response.model.data = values
+    response.status(200)
+    next()
+  }, function (errors) {
+    console.log(errors)
+  })
 }
 
-let getOverallRescueCount = function () {
-  Rescue.aggregate([
-    // Grouping pipeline
-    {
-      '$group': {
-        _id: {
-          month: {
-            $month: '$createdAt'
-          },
-          day: {
-            $dayOfMonth: '$createdAt'
-          },
-          year: {
-            $year: '$createdAt'
-          }
-        },
-        count: {
-          $sum: 1
-        }
-      }
+let groupByDateAggregator = {
+  '$group': {
+    _id: {
+      month: {
+        $month: '$createdAt'
+      },
+      day: {
+        $dayOfMonth: '$createdAt'
+      },
+      year: {
+        $year: '$createdAt'
+      },
+      successful: '$successful'
+    },
+    count: {
+      $sum: 1
     }
-  ], function (error, result) {
-    console.log(error)
-    console.log(result)
+  }
+}
+
+
+let getOverallRescueCount = function () {
+  return new Promise(function (resolve, reject) {
+    Rescue.aggregate([
+      groupByDateAggregator
+    ]).exec().then(function (objs) {
+      let organisedCollection = []
+      for (let obj of objs) {
+        let date = Date.parse(`${obj._id.year}-${obj._id.month}-${obj._id.day}`)
+        let days = organisedCollection.filter(function (el) {
+          return el.date === date
+        })
+
+        let day
+        if (days.length === 0) {
+          day = { date: date, success: 0, failure: 0 }
+        } else {
+          day = days[0]
+          organisedCollection.splice(organisedCollection.indexOf(day), 1)
+        }
+
+        if (obj._id.successful === true) {
+          day.success = obj.count
+        } else {
+          day.failure = obj.count
+        }
+        organisedCollection.push(day)
+      }
+      organisedCollection = organisedCollection.sort(function (x, y) {
+        return y - x
+      })
+      resolve(organisedCollection)
+    }, function (errors) {
+      reject(errors)
+    })
   })
 }
--- static/leaderboard.js
+++ static/leaderboard.js
@@ -1,4 +1,67 @@
 'use strict'
-$(function () {
+/* global d3 */
 
+$(function () {
+  generateRescueGraph()
 })
+
+function generateRescueGraph () {
+  d3.json('/statistics', function (error, json) {
+    var x, y, xAxis, yAxis, margin, height, width, colour, svg, rescue, legend, data
+
+    data = json.data
+
+    if (!error) {
+      margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 40
+      },
+      width = 960 - margin.left - margin.right
+      height = 500 - margin.top - margin.bottom
+
+
+      x = d3.scale.ordinal().rangeRoundBands([0, width], 0.1)
+      y = d3.scale.linear().rangeRound([height, 0])
+      xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(d3.time.months, 1)
+        .tickFormat(d3.time.format('%Y-%m-%d')).tickSize(0).tickPadding(8)
+      yAxis = d3.svg.axis().scale(y).orient('left').tickFormat(d3.format('.2s'))
+
+      colour = d3.scale.ordinal().range(['#4bb47f', '#e87575'])
+
+      svg = d3.select('body').append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom).append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+      colour.domain(['Success', 'Failure'])
+
+      x.domain(data.map(function (d) { return new Date(d.date) }))
+      y.domain([0, d3.max(data, function (d) { return d.success + d.failure })])
+
+      svg.append('g').attr('class', 'x axis').attr('transform', 'translate(0,' + height + ')').call(xAxis)
+      svg.append('g').attr('class', 'y axis').call(yAxis).append('text')
+        .attr('transform', 'rotate(-90)').attr('y', 6)
+        .attr('dy', '.71em').style('text-anchor', 'end').text('Rescues')
+
+      rescue = svg.selectAll('.rescue').data(data).enter().append('g').attr('class', 'g')
+        .attr('transform', function (d) { return 'translate(' + x(new Date(d.date)) + ',0)' })
+
+      rescue.selectAll('g').data(function (d) { return [d.success, d.failure] }).enter().append('rect')
+        .attr('width', x.rangeBand()).attr('y', function (d) { return y(d) })
+        .attr('height', function (d) { y(d) })
+        .style('fill', function () { return colour(0) })
+
+      legend = svg.selectAll('.legend').data(colour.domain().slice().reverse()).enter().append('g')
+      .attr('class', 'legend').attr('transform', function (d, i) { return 'translate(0,' + i * 20 + ')' })
+
+      legend.append('rect').attr('x', width - 18).attr('width', 18).attr('height', 18).style('fill', colour)
+
+      legend.append('text').attr('x', width - 24).attr('y', 9).attr('dy', '.35em')
+      .style('text-anchor', 'end').text(function (d) { return d })
+    } else {
+      console.log(error)
+    }
+  })
+}
