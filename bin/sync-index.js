67763e44684364b97b647e45a15cda516ae8e02a
Trezy <tre@trezy.com>
2016-02-29 21:01:34 -0600

    Update syncing script

--- bin/sync-index.js
+++ bin/sync-index.js
@@ -6,15 +6,48 @@ let Rescue = require('../api/models/rescue')
 let User = require('../api/models/user')
 let mongoose = require('mongoose')
 
+let count = 0
+
 mongoose.connect('mongodb://localhost/fuelrats')
 
 winston.info('Beginning Index Model Sync')
+
 winston.info('Syncing Rats')
-Rat.synchronize()
+let ratStream = Rat.synchronize()
+ratStream.on('data', function (error, doc) {
+  count++
+})
+ratStream.on('error', function (error) {
+  console.error(error)
+})
+ratStream.on('close', function () {
+  console.log('Finished syncing Rats')
+})
 
 winston.info('Syncing Rescue')
 Rescue.synchronize()
+let rescueStream = Rescue.synchronize()
+rescueStream.on('data', function (error, doc) {
+  count++
+})
+rescueStream.on('error', function (error) {
+  console.error(error)
+})
+rescueStream.on('close', function () {
+  console.log('Finished syncing Rescues')
+})
 
 winston.info('Syncing User')
 User.synchronize()
+let userStream = User.synchronize()
+userStream.on('data', function (error, doc) {
+  count++
+})
+userStream.on('error', function (error) {
+  console.error(error)
+})
+userStream.on('close', function () {
+  console.log('Finished syncing Users')
+})
+
 winston.info('Index sync complete')
