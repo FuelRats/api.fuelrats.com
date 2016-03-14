a39b4cf37c315e3f4282de30543e20e4da0ce6be
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-03-14 17:22:59 +0100

    Ensure all dependencies are running before executing tests

--- grunt/express.js
+++ grunt/express.js
@@ -13,7 +13,8 @@ module.exports = {
   dev: {
     options: {
       port: config.port,
-      script: 'index.js'
+      script: 'index.js',
+      output: '(Listening for requests on port)'
     }
   }
 }
