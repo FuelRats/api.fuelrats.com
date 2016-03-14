db5f87044a02d8ca6680de4527dc43c6b5b4bac8
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-03-14 16:38:48 +0100

    Change incorrect HTTP status codes identified through unit testing

--- api/controllers/rat.js
+++ api/controllers/rat.js
@@ -14,7 +14,7 @@ exports.get = function (request, response, next) {
 
     response.model.data = data
     response.model.meta = meta
-    response.status = 400
+    response.status = 200
     next()
   }, function (error) {
     response.model.errors.push(error.error)
@@ -177,7 +177,7 @@ exports.put = function (request, response, next) {
 
   exports.update(request.body, {}, request.params).then(function (data) {
     response.model.data = data.data
-    response.status(201)
+    response.status(200)
     next()
   }, function (error) {
     response.model.errors.push(error.error)
