a1837494aea0a3c456f8bbd624519bdd475b2b92
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-04-06 22:04:19 +0200

    Prevent exception on databases without firstLimpet field

--- api/controllers/welcome.js
+++ api/controllers/welcome.js
@@ -33,7 +33,7 @@ exports.get = function (request, response) {
               rescue.lastModified = moment(rescue.lastModified)
 
 
-              if (rescue.firstLimpet.toString() !== CMDR.id) {
+              if (rescue.firstLimpet && rescue.firstLimpet.toString() !== CMDR.id) {
                 rescue.assist = true
               }
             })
