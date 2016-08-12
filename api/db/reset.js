cd05257a2e73faafac5df63c23ad6114f6163c80
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-08-12 12:06:37 +0200

    Migrate password reset to postgres

---
+++ api/db/reset.js
@@ -0,0 +1,27 @@
+'use strict'
+
+module.exports = function (sequelize, DataTypes) {
+  let Reset = sequelize.define('Reset', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    value: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    expires: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    classMethods: {
+      associate: function (models) {
+        Reset.belongsTo(models.User, { as: 'user' })
+      }
+    }
+  })
+
+  return Reset
+}
