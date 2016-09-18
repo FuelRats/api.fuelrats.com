b2805cd1a8c505847248b570ff2b5a2d898f3e1b
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-09-18 20:00:50 +0200

    Fix IPs not always logging properly

--- index.js
+++ index.js
@@ -218,8 +218,8 @@ if (options.logging || options.test) {
     winston.info('ENDPOINT:', request.originalUrl)
     winston.info('METHOD:', request.method)
     winston.info('DATA:', censoredParams)
-    winston.info('IP:', request.headers['X-Forwarded-for'] || request.connection.remoteAddress)
-    request.inet = request.headers['X-Forwarded-for'] || request.connection.remoteAddress
+    winston.info('IP:', request.headers['x-forwarded-for'] || request.connection.remoteAddress)
+    request.inet = request.headers['x-forwarded-for'] || request.connection.remoteAddress
     next()
   })
 }
