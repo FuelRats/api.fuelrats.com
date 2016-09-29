15f170abebd239e9a1c0c39dfc10aeeeaa3caab1
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-09-29 15:27:48 +0200

    Update lets encrypt certificate location to correct relative path

--- api/Anope/index.js
+++ api/Anope/index.js
@@ -2,10 +2,10 @@
 
 let xmlrpc = require('homematic-xmlrpc')
 let sslRootCAs = require('ssl-root-cas/latest')
-  .addFile(__dirname + '/../ca/lets-encrypt-x1-cross-signed.pem')
-  .addFile(__dirname + '/../ca/lets-encrypt-x2-cross-signed.pem')
-  .addFile(__dirname + '/../ca/lets-encrypt-x3-cross-signed.pem')
-  .addFile(__dirname + '/../ca/lets-encrypt-x4-cross-signed.pem')
+  .addFile(__dirname + '../../ca/lets-encrypt-x1-cross-signed.pem')
+  .addFile(__dirname + '../../ca/lets-encrypt-x2-cross-signed.pem')
+  .addFile(__dirname + '../../ca/lets-encrypt-x3-cross-signed.pem')
+  .addFile(__dirname + '../../ca/lets-encrypt-x4-cross-signed.pem')
 sslRootCAs.inject()
 
 const client = xmlrpc.createSecureClient('https://irc.eu.fuelrats.com:6080/xmlrpc')
