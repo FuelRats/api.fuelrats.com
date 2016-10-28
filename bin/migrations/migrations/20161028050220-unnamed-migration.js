7c83c419e27de90b8a58151303fffab7fb9e030d
Alex S. Glomsaas <alex@sorlie.co.uk>
2016-10-28 07:13:40 +0200

    Create database migration for rescues table

---
+++ bin/migrations/migrations/20161028050220-unnamed-migration.js
@@ -0,0 +1,31 @@
+'use strict'
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    queryInterface.addColumn(
+      'Rescues',
+      'type',
+      {
+        type: Sequelize.ENUM('success', 'failure', 'invalid', 'other'),
+        allowNull: false,
+        defaultValue: 'other'
+      }
+    )
+
+
+    queryInterface.addColumn(
+      'Rescues',
+      'status',
+      {
+        type: Sequelize.ENUM('open', 'inactive', 'closed'),
+        allowNull: false,
+        defaultValue: 'other'
+      }
+    )
+  },
+
+  down: function (queryInterface) {
+    queryInterface.removeColumn('Rescues', 'type')
+    queryInterface.removeColumn('Rescues', 'status')
+  }
+}
