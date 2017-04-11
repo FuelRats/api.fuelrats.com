523df2b2930e9ec71e020b08a2ad27fa527777b2
Alex S. Glomsaas <alex@sorlie.co.uk>
2017-04-11 16:53:08 +0200

    Add shipType column to ship registry

--- api/db/ship.js
+++ api/db/ship.js
@@ -16,6 +16,43 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false,
       unique: true,
       autoIncrement: true
+    },
+    shipType: {
+      type: DataTypes.ENUM(
+        'Adder',
+        'Anaconda',
+        'Asp Explorer',
+        'Asp Scout',
+        'Beluga Liner',
+        'Cobra MkIII',
+        'Cobra MkIV',
+        'Diamondback Explorer',
+        'Diamondback Scout',
+        'Eagle',
+        'F63 Condor',
+        'Federal Assault Ship',
+        'Federal Corvette',
+        'Federal Dropship',
+        'Federal Gunship',
+        'Fer-de-lance',
+        'Hauler',
+        'Imperial Clipper',
+        'Imperial Courier',
+        'Imperial Cutter',
+        'Imperial Eagle',
+        'Imperial Fighter',
+        'Keelback',
+        'Orca',
+        'Python',
+        'Sidewinder MkI',
+        'Taipan Fighter',
+        'Type-6 Transporter',
+        'Type-7 Transporter',
+        'Type 9 Heavy',
+        'Viper MkIII',
+        'Viper MkIV',
+        'Vulture'
+      )
     }
   }, {
     paranoid: true,
