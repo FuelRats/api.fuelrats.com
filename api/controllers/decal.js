8fa1328dbd59e876ce97042d1c4d7192503eb04c
Alex S. Glomsaas <alex@sorlie.co.uk>
2017-04-26 02:08:19 +0200

    Add ability for admins to query user egilibity for decals

--- api/controllers/decal.js
+++ api/controllers/decal.js
@@ -2,20 +2,53 @@
 
 let Errors = require('../errors')
 let Decal = require('../classes/Decal')
+let User = require('../db').User
+let Permission = require('../permission')
 
 class HTTP {
   static check (request, response, next) {
-    Decal.checkEligble(request.user).then(function () {
-      response.model.data = {
-        eligble: true
-      }
-      response.status = 200
-      next()
-    }).catch(function (error) {
-      response.model.errors.push(error)
-      response.status(error.code)
-      next()
-    })
+    if (Object.keys(request.query).length > 0) {
+      Permission.require('user.read', request.user).then(function () {
+        User.findOne({
+          where: request.query
+        }).then(function (user) {
+          if (!user) {
+            let error = Error.throw('not_found', 'user')
+            response.model.errors.push(error)
+            response.status(error.code)
+            next()
+          }
+
+          Decal.checkEligble(user).then(function () {
+            response.model.data = {
+              eligble: true
+            }
+            response.status = 200
+            next()
+          }).catch(function (error) {
+            response.model.errors.push(error)
+            response.status(error.code)
+            next()
+          })
+        })
+      }).catch(function (error) {
+        response.model.errors.push(error)
+        response.status(error.error.code)
+        next()
+      })
+    } else {
+      Decal.checkEligble(request.user).then(function () {
+        response.model.data = {
+          eligble: true
+        }
+        response.status = 200
+        next()
+      }).catch(function (error) {
+        response.model.errors.push(error)
+        response.status(error.code)
+        next()
+      })
+    }
   }
 
   static redeem (request, response, next) {
