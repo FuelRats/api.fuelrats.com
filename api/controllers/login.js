6c497b2ecf3c371214322d4d19ff1d62f2a4824b
Alex S. Glomsaas <alex@sorlie.co.uk>
2017-07-02 21:50:20 +0200

    Implement login endpoint in koa

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -1,21 +1,19 @@
 'use strict'
 const Action = require('../db').Action
+const Authentication = require('./auth')
+const Error = require('../errors')
 
+class Login {
+  static async login (ctx, next) {
+    let user = await Authentication.passwordAuthenticate(ctx.data.email, ctx.data.password)
+    if (!user) {
+      throw Error.template('not_authenticated', 'Authentication failed')
+    }
 
-exports.post = function (request, response, next) {
-  if (request.user) {
-    Action.create({
-      inet: request.inet,
-      type: 'login',
-      userId: request.user.id
-    })
-    response.model.data = request.user
-    response.status(200)
-    next()
-  } else {
-    request.session.userIp = request.headers['x-forwarded-for'] || request.connection.remoteAddress
-
-    request.session.errorCode = 401 // This could signify that the login has failed
-    response.redirect('/login?error_login=1')
+    ctx.session.userId = user.data.id
+    ctx.status = 200
+    return user
   }
 }
+
+module.exports = Login
