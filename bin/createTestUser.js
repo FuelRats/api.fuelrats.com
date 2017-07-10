75ce2d2aca76db50d37ab65a8cd503a7948da57c
Alex S. Glomsaas <alex@sorlie.co.uk>
2017-07-10 15:34:51 +0200

    Rewrite unit test user generation script to use async/await

--- bin/createTestUser.js
+++ bin/createTestUser.js
@@ -4,64 +4,50 @@ process.env.NODE_ENV = 'testing'
 let db = require('../api/db').db
 let User = require('../api/db').User
 let Client = require('../api/db').Client
+let Group = require('../api/db').Group
 let bcrypt = require('bcrypt')
-let winston = require('winston')
 let fs = require('fs')
 let crypto = require('crypto')
 
 
-db.sync({ force: true }).then(function () {
-  bcrypt.hash('testuser', 16, function (error, hash) {
-    let adminTestUser = {
-      email: 'admintestuser@fuelrats.com',
-      password: hash,
-      nicknames: db.literal('ARRAY[\'admintestnick\']::citext[]'),
-      groups: ['rat', 'dispatch', 'admin']
-    }
+db.sync({ force: true }).then(async function () {
+  let hash = await bcrypt.hash('testuser', 16)
+  let adminTestUser = {
+    email: 'admintestuser@fuelrats.com',
+    password: hash,
+    nicknames: db.literal('ARRAY[\'admintestnick\']::citext[]')
+  }
 
-    User.create(adminTestUser).then(function (adminUser) {
-      winston.info('Admin Test User Created')
+  let adminUser = await User.create(adminTestUser)
 
-      let secret = crypto.randomBytes(24).toString('hex')
+  console.log('Admin Test User Created')
+  adminUser.addGroup()
+  let secret = await crypto.randomBytes(24).toString('hex')
 
-      bcrypt.hash(secret, 16, function (error, hash) {
-        if (error) {
-          return winston.error(error)
-        }
+  bcrypt.hash(secret, 16)
 
-        Client.create({
-          name: 'API Test Client',
-          userId: adminUser.id,
-          secret: hash
-        }).then(function (client) {
-          client.secret = secret
-          fs.writeFile('testinfo.json', JSON.stringify(client.toJSON()), function (err) {
-            if (err) {
-              return winston.error(err)
-            }
-
-            winston.info('Test data file written')
-          })
-        }).catch(function (error) {
-          winston.error(error)
-        })
-      })
-    }).catch(function (error) {
-      winston.error(error)
-    })
-
-    let testUser = {
-      email: 'testuser@fuelrats.com',
-      password: hash,
-      nicknames: db.literal('ARRAY[\'testnick\']::citext[]'),
-      groups: []
-    }
-
-    User.create(testUser).then(function () {
-      winston.info('Test User Created')
-    }).catch(function (error) {
-      winston.error(error)
-    })
+  let client = Client.create({
+    name: 'API Test Client',
+    userId: adminUser.id,
+    secret: hash
   })
 
+  fs.writeFile('testinfo.json', JSON.stringify(client.toJSON()), function (err) {
+    if (err) {
+      return console.log(err)
+    }
+
+    console.log('Test data file written')
+  })
+
+
+  let testUser = {
+    email: 'testuser@fuelrats.com',
+    password: hash,
+    nicknames: db.literal('ARRAY[\'testnick\']::citext[]'),
+    groups: []
+  }
+
+  await User.create(testUser)
+  console.log('Test User Created')
 })
