5e7737f5a31d485c7e1a7e07bd9717c89f604a01
Alex S. Glomsaas <alex@sorlie.co.uk>
2017-09-30 16:06:57 +0200

    Default offset to 0 not null

--- api/websocket.js
+++ api/websocket.js
@@ -177,14 +177,14 @@ class WebSocketManager {
       if (Array.isArray(result)) {
         meta.meta = {
           count: result.length,
-          limit: query._limit || null,
-          offset: query._offset || null,
+          limit: query._limit || 0,
+          offset: query._offset || 0,
         }
       } else {
         meta.meta = {
           count: result.rows.length,
-          limit: query._limit || null,
-          offset: query._offset || null,
+          limit: query._limit || 0,
+          offset: query._offset || 0,
           total: result.count
         }
       }
