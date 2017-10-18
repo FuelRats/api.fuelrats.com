a87de9c3a230a0faa7581c2471a8eee47c0ba765
Scott Deakin <sdeaks@gmail.com>
2017-10-18 11:31:14 +0100

    added rescue stats test

--- test/all.js
+++ test/all.js
@@ -5,7 +5,7 @@ const { join } = require('path')
 
 // we want to explicity run the test in order of complexity
 // not their alphabetical order 
-const tests = ['login.js', 'rescue.js']
+const tests = ['login.js', 'rescue.js', 'stats.js']
 
 /**
  * run against all the files/dirs defined in tests
