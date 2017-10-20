769a5bde7f57a444491bf39ef378420354e9575a
Alex S. Glomsaas <alex@sorlie.co.uk>
2017-10-20 22:34:32 +0200

    Move function for getting display name to an easily accessible function on the database layer

--- api/controllers/jira/drill.js
+++ api/controllers/jira/drill.js
@@ -71,8 +71,10 @@ class JiraDrill {
     })
 
     let userResponse = UsersPresenter.render(userInstance, {})
-    await HostServ.update(userResponse)
-    BotServ.say('#doersofstuff', `[API] Drilled status and IRC permissions updated for ${user.email}`)
+    //await HostServ.update(userResponse)
+
+    let displayRat = User.preferredRat(userResponse)
+    BotServ.say('#doersofstuff', `[API] Drilled status and IRC permissions updated for ${displayRat.attributes.name} (user ${user.id})`)
     return userResponse
   }
 }
