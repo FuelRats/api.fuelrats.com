2347f37b1f1fc7a9b351a2d7ad215c26419b8acf
Alex S. Glomsaas <alex@sorlie.co.uk>
2018-01-22 21:58:49 +0100

    Don't expose whether reset email is actually sent

--- src/routes/Resets.js
+++ src/routes/Resets.js
@@ -27,50 +27,14 @@ export default class Resets extends API {
       }
     })
 
-    if (!user) {
-      throw new NotFoundAPIError({ pointer: '/data/attributes/email' })
-    }
-
-    let resets = await Reset.findAll({
-      where: {
-        userId: user.id
-      }
-    })
-
-    let requiredReset = false
-
-    resets.map((reset) => {
-      if (reset.required) {
-        requiredReset = true
-      }
-      reset.destroy()
-    })
-
-    let reset = await Reset.create({
-      value: crypto.randomBytes(global.RESET_TOKEN_LENGTH / 2).toString('hex'),
-      expires: new Date(Date.now() + EXPIRE_LENGTH).getTime(),
-      userId: user.id,
-      required: requiredReset
-    })
-
     ctx.state.writeResp = false
-    let html = await ctx.render('reset-email', {
-      resetlink: Resets.getResetLink(reset.value)
-    })
 
-    let transporter = nodemailer.createTransport('smtp://orthanc.localecho.net')
-    try {
-      await transporter.sendMail({
-        from: 'Fuel Rats (Do Not Reply) <blackhole@fuelrats.com>',
-        to: user.email,
-        subject: 'Fuel Rats Password Reset Requested',
-        text: Resets.getPlainTextEmail(reset.value),
-        html: html
+    if (user) {
+      let reset = getReset(user, ctx.data.required)
+      let html = await ctx.render('reset-email', {
+        resetlink: Resets.getResetLink(reset.value)
       })
-      BotServ.say(global.TECHNICAL_CHANNEL, `[API] Password reset for ${user.email} requested by ${ctx.inet}`)
-    } catch (ex) {
-      BotServ.say(global.TECHNICAL_CHANNEL, '[API] Password reset failed due to error from SMTP server')
-      return
+      await sendReset(ctx, user, reset, html)
     }
 
     ctx.body = 'OK'
@@ -93,22 +57,7 @@ export default class Resets extends API {
       throw new NotFoundAPIError({ pointer: '/data/attributes/email' })
     }
 
-    let resets = await Reset.findAll({
-      where: {
-        userId: user.id
-      }
-    })
-
-    resets.map((reset) => {
-      reset.destroy()
-    })
-
-    let reset = await Reset.create({
-      value: crypto.randomBytes(global.RESET_TOKEN_LENGTH).toString('hex'),
-      expires: new Date(Date.now() + EXPIRE_LENGTH).getTime(),
-      userId: user.id,
-      required: ctx.data.required || false
-    })
+    let reset = await getReset(user, ctx.data.required)
 
     ctx.response.status = 201
     return Resets.presenter.render(reset, API.meta(reset))
@@ -187,5 +136,54 @@ export default class Resets extends API {
     }
     ResetsPresenter.prototype.type = 'resets'
     return ResetsPresenter
+  }
+}
+
+/**
+ * Get a reset key for a user
+ * @param user the user to request a reset key for
+ * @param required wether this reset is required before the user can access the account
+ * @returns {Promise<void>}
+ */
+async function getReset (user, required = false) {
+  let resets = await Reset.findAll({
+    where: {
+      userId: user.id
+    }
+  })
+
+  resets.map((reset) => {
+    reset.destroy()
+  })
+
+  return Reset.create({
+    value: crypto.randomBytes(global.RESET_TOKEN_LENGTH).toString('hex'),
+    expires: new Date(Date.now() + EXPIRE_LENGTH).getTime(),
+    userId: user.id,
+    required: required
+  })
+}
+
+/**
+ * Send a Reset Email
+ * @param ctx the request context
+ * @param user the user to send the email to
+ * @param reset the reset object to use
+ * @param html the HTML to use for the email
+ * @returns {Promise<void>}
+ */
+async function sendReset (ctx, user, reset, html) {
+  let transporter = nodemailer.createTransport('smtp://orthanc.localecho.net')
+  try {
+    await transporter.sendMail({
+      from: 'Fuel Rats (Do Not Reply) <blackhole@fuelrats.com>',
+      to: user.email,
+      subject: 'Fuel Rats Password Reset Requested',
+      text: Resets.getPlainTextEmail(reset.value),
+      html: html
+    })
+    BotServ.say(global.TECHNICAL_CHANNEL, `[API] Password reset for ${user.email} requested by ${ctx.inet}`)
+  } catch (ex) {
+    BotServ.say(global.TECHNICAL_CHANNEL, '[API] Password reset failed due to error from SMTP server')
   }
 }
