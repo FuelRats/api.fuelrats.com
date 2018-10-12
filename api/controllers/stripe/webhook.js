dff83ecac38c2f06885c28fc00353812c2c93dcc
Alex Sørlie Glomsaas <alex@sorlie.eu>
2018-10-12 20:51:24 +0200

    Fix logic issue that was causing tracking link generation to fail

--- api/controllers/stripe/webhook.js
+++ api/controllers/stripe/webhook.js
@@ -140,16 +140,12 @@ function getTrackingLink (number) {
     return null
   }
 
-  switch (number) {
-    case royalMailTrackingCode.test(number):
-      return `https://www.royalmail.com/portal/rm/track?trackNumber=${number}`
-
-    case parcelForceTrackingCode.test(number):
-      return `http://www.parcelforce.com/portal/pw/track?trackNumber=${number}`
-
-    default:
-      return null
+  if (royalMailTrackingCode.test(number) === true) {
+    return `https://www.royalmail.com/portal/rm/track?trackNumber=${number}`
+  } else if (parcelForceTrackingCode.test(number) === true) {
+    return `http://www.parcelforce.com/portal/pw/track?trackNumber=${number}`
   }
+  return null
 }
 
 module.exports = Webhook
