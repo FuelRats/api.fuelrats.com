5342a3b5703223a2688a0ce931cc9c118786f494
Alex Sørlie Glomsaas <alex@sorlie.io>
2019-07-13 12:58:49 +0200

    Add 'CMDR' to list of phrases in rat names where the API will tell you to fuck off

--- src/classes/Validators.js
+++ src/classes/Validators.js
@@ -5,7 +5,7 @@ import { URL } from 'url'
 
 export const IRCVirtualHost = /^[a-z][a-z0-9.]{3,64}$/u
 export const IRCNickname = /^[A-Za-z_\\`\[\]{}]([A-Za-z0-9_\\`\[\]{}]{1,29})?$/u
-const forbiddenCMDRNameComponents = ['[pc]', '[xb]', '[ps]']
+const forbiddenCMDRNameComponents = ['[pc]', '[xb]', '[ps]', 'CMDR']
 
 // language=JSUnicodeRegexp
 export const FrontierRedeemCode = new RegexLiteral(`^
