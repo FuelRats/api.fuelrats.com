a45bce34b0ed1903b52113f481fb8f5197cbb331
Alex Sørlie Glomsaas <alex@sorlie.io>
2019-12-23 22:14:14 +0100

    Fix an issue where searching for a nick without a corresponding user causes an exception resolves API-310

--- src/view/NicknameView.js
+++ src/view/NicknameView.js
@@ -53,10 +53,13 @@ export default class NicknameView extends DatabaseView {
    * @inheritdoc
    */
   get isSelf () {
-    if (this.query.connection.state.user && this.object.user.id === this.query.connection.state.user.id) {
-      return this.query.connection.state.permissions.includes('nicknames.read.me')
+    if (!this.query.connection.state.user || !this.object.user) {
+      return false
     }
-    return false
+    if (this.object.user.id !== this.query.connection.state.user.id) {
+      return false
+    }
+    return this.query.connection.state.permissions.includes('nicknames.read.me')
   }
 
   /**
