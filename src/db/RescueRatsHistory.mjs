67dae776bec0cb021e5f5bbaa5afc8929a988bd7
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-02-19 19:46:48 +0100

    Duplicate all the fields in revision tables because using inheritance makes Sequelize go bananas

--- src/db/RescueRatsHistory.mjs
+++ src/db/RescueRatsHistory.mjs
@@ -1,8 +1,7 @@
-import { column, validate, type, table } from './Model'
-import RescueRats from './RescueRats'
+import Model, { column, validate, type, table } from './Model'
 
 @table({ tableName: 'rescueratshistory' })
-export default class RescueRatsHistory extends RescueRats {
+export default class RescueRatsHistory extends Model {
   /*  Override the ID field from rescues to not be set as a primary key,
   since the history contains multiple versions of the same rescue */
   @validate({ isUUID: 4 })
@@ -11,6 +10,21 @@ export default class RescueRatsHistory extends RescueRats {
 
   @column(type.DATE, { primaryKey: true })
   static updatedAt = type.NOW
+
+  @column(type.UUID)
+  static rescueId = undefined
+
+  @column(type.UUID)
+  static ratId = undefined
+
+  @column(type.UUID, { allowNull: true })
+  static assignerUserId = undefined
+
+  @column(type.UUID, { allowNull: true })
+  static assignerClientId = undefined
+
+  @column(type.RANGE(type.DATE))
+  static temporalPeriod = [type.now, undefined]
 
   /**
    * @inheritdoc
