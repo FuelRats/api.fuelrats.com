35caba7796c1b6630881b15b4ef5da20d855e927
Alex Sørlie Glomsaas <alex@sorlie.io>
2020-04-13 22:53:57 +0200

    Sigh

--- src/classes/TrafficControl.mjs
+++ src/classes/TrafficControl.mjs
@@ -177,7 +177,7 @@ class AuthenticatedUserEntity extends TrafficEntity {
    * @returns {number} total number of requests
    */
   get totalRequests () {
-    return this.userRateLimit || allowedAuthenticatedRequestCount
+    return this.userRateLimit ?? allowedAuthenticatedRequestCount
   }
 }
 
