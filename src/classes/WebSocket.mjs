7c937e3683890c5261d01756df991be8777a497a
Alex S. Glomsaas <alex@sorlie.io>
2021-05-06 20:40:07 +0200

    Yet another websocket fix..

--- src/classes/WebSocket.mjs
+++ src/classes/WebSocket.mjs
@@ -65,7 +65,10 @@ export default class WebSocket {
 
 
       const url = new URL(`${config.server.externalUrl}${req.url}`)
-      const bearer = url.searchParams.get('bearer')
+      let bearer = url.searchParams.get('bearer')
+      if (!bearer) {
+        bearer = req.headers['x-bearer']
+      }
       if (bearer) {
         const { user, scope, clientId } = await Authentication.bearerAuthenticate({ bearer })
         if (user) {
