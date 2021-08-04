52c1f218becadf8a655bc62e7f7d0cfa05d5f671
Cameron Welter <camjwelt@gmail.com>
2021-08-04 19:47:06 +0000

    re-implement config validation as it's own helper module. Add validators and type conversions for various

--- src/config.mjs
+++ src/config.mjs
@@ -1,39 +1,48 @@
-/* eslint-disable no-magic-numbers,no-console */
-let configErrors = 0
-let configWarnings = 0
+/* eslint-disable no-magic-numbers */
+import {
+  required,
+  optional,
+  recommended,
+  ensureValidConfig,
+  isBoolean,
+  isArray,
+  isIrcChannel,
+  isNumber,
+  isUUID,
+} from './helpers/ConfigValidators'
 
 const config = {
   server: {
     hostname: required('FRAPI_HOSTNAME', [], 'localhost'),
-    port: required('FRAPI_PORT', [], 8080),
+    port: required('FRAPI_PORT', [isNumber], 8080),
     externalUrl: required('FRAPI_URL', [], 'http://localhost:8080'),
-    cookieSecret: required('FRAPI_COOKIE', []),
-    proxyEnabled: required('FRAPI_PROXY_ENABLED', [], false),
-    whitelist: optional('FRAPI_WHITELIST', [], []),
+    cookieSecret: required('FRAPI_COOKIE', [], undefined),
+    proxyEnabled: required('FRAPI_PROXY_ENABLED', [isBoolean], false),
+    whitelist: optional('FRAPI_WHITELIST', [isArray], []),
   },
   documentationUrl: required('FRAPI_DOCUMENTATION', [], 'https://github.com/FuelRats/FuelRats-API-Docs/blob/master/beta.md'),
   frontend: {
-    clientId: recommended('FRAPI_FRONTEND_CLIENTID', []),
+    clientId: recommended('FRAPI_FRONTEND_CLIENTID', [isUUID], undefined),
     url: required('FRAPI_FRONTEND_URL', [], 'https://fuelrats.com'),
   },
   postgres: {
     database: required('FRAPI_POSTGRES_DATABASE', [], 'fuelrats'),
     hostname: required('FRAPI_POSTGRES_HOSTNAME', [], 'localhost'),
-    port: required('FRAPI_POSTGRES_PORT', [], 5432),
+    port: required('FRAPI_POSTGRES_PORT', [isNumber], 5432),
     username: required('FRAPI_POSTGRES_USERNAME', [], 'fuelrats'),
     password: optional('FRAPI_POSTGRES_PASSWORD', [], undefined),
   },
   anope: {
     database: required('FRAPI_ANOPE_DATABASE', [], 'anope'),
     hostname: required('FRAPI_ANOPE_HOSTNAME', [], 'localhost'),
-    port: required('FRAPI_ANOPE_PORT', [], 3306),
+    port: required('FRAPI_ANOPE_PORT', [isNumber], 3306),
     username: required('FRAPI_ANOPE_USERNAME', [], 'anope'),
     password: optional('FRAPI_ANOPE_PASSWORD', [], undefined),
   },
   irc: {
     server: recommended('FRAPI_IRC_SERVER', [], undefined),
     serverName: recommended('FRAPI_IRC_SERVERNAME', [], undefined),
-    port: recommended('FRAPI_IRC_PORT', [], 6900),
+    port: recommended('FRAPI_IRC_PORT', [isNumber], 6900),
     password: recommended('FRAPI_IRC_PASSWORD', [], undefined),
   },
   geoip: {
@@ -43,11 +52,11 @@ const config = {
     url: recommended('FRAPI_ANNOUNCER,URL', [], 'https://announcer-dev.fuelrats.com/api'),
     secret: recommended('FRAPI_ANNOUNCER_SECRET', [], undefined),
     destinations: {
-      rescue: recommended('FRAPI_ANNOUNCER_DESTINATION_RESCUE', [], '#ratchat'),
-      moderation: recommended('FRAPI_ANNOUNCER_DESTINATION_MODERATION', [], '#rat-ops'),
-      network: recommended('FRAPI_ANNOUNCER_DESTINATION_NETWORK', [], '#opers'),
-      technical: recommended('FRAPI_ANNOUNCER_DESTINATION_TECHNICAL', [], '#rattech'),
-      drill: recommended('FRAPI_ANNOUNCER_DESTINATION_DRILL', [], '#doersofstuff'),
+      rescue: recommended('FRAPI_ANNOUNCER_DESTINATION_RESCUE', [isIrcChannel], '#ratchat'),
+      moderation: recommended('FRAPI_ANNOUNCER_DESTINATION_MODERATION', [isIrcChannel], '#rat-ops'),
+      network: recommended('FRAPI_ANNOUNCER_DESTINATION_NETWORK', [isIrcChannel], '#opers'),
+      technical: recommended('FRAPI_ANNOUNCER_DESTINATION_TECHNICAL', [isIrcChannel], '#rattech'),
+      drill: recommended('FRAPI_ANNOUNCER_DESTINATION_DRILL', [isIrcChannel], '#doersofstuff'),
     },
   },
   frontier: {
@@ -57,7 +66,7 @@ const config = {
   },
   graylog: {
     host: recommended('FRAPI_GRAYLOG_HOST', [], undefined),
-    port: recommended('FRAPI_GRAYLOG_PORT', [], 12201),
+    port: recommended('FRAPI_GRAYLOG_PORT', [isNumber], 12201),
     facility: recommended('FRAPI_GRAYLOG_FACILITY', [], 'fuelratsapi'),
   },
   slack: {
@@ -66,7 +75,7 @@ const config = {
   },
   smtp: {
     hostname: recommended('FRAPI_SMTP_HOSTNAME', [], 'smtp-relay.gmail.com'),
-    port: recommended('FRAPI_SMTP_PORT', [], 587),
+    port: recommended('FRAPI_SMTP_PORT', [isNumber], 587),
     sender: recommended('FRAPI_SMTP_SENDER', [], 'blackhole@fuelrats.com'),
   },
   jira: {
@@ -81,85 +90,10 @@ const config = {
   },
 }
 
-console.log(`${configErrors} config errors.`)
-console.log(`${configWarnings} config warnings.`)
-if (configErrors > 0) {
-  console.log('FATAL CONFIGURATION PROBLEMS DETECTED, EXITING')
-  process.abort()
-}
+ensureValidConfig()
 
-/**
- * Require a config environment variable. This setting will cause a fatal error if invalid
- * @param {string} property The environment variable name
- * @param {[Function]} validations array of validation functions to run
- * @param {*} defaultValue default value to be provided for this, if any
- * @returns {*|undefined}
- */
-function required (property, validations = [], defaultValue = undefined) {
-  const value = process.env[property] ?? defaultValue
-  if (typeof value === 'undefined') {
-    console.error(`FATAL ERROR: Required config parameter ${property} is not provided`)
-    configErrors += 1
-    return undefined
-  }
 
-  validations.forEach((validation) => {
-    const validationMessage = validation.call(value, value)
-    if (validationMessage !== true) {
-      console.error(`FATAL ERROR: Required config parameter ${property} did not pass validation. ${validationMessage}`)
-      configErrors += 1
-    }
-  })
 
-  return value
-}
 
-/**
- * Set a recommended config environment variable. This setting will generate a warning if invalid
- * @param {string} property The environment variable name
- * @param {[Function]} validations array of validation functions to run
- * @param {*} defaultValue default value to be provided for this, if any
- * @returns {*|undefined}
- */
-function recommended (property, validations = [], defaultValue = undefined) {
-  const value = process.env[property] ?? defaultValue
-  if (typeof value === 'undefined') {
-    console.error(`WARNING: Config parameter ${property} is not configured, 
-    functionality will be limited and/or unstable`)
-    configWarnings += 1
-    return undefined
-  }
-
-  validations.forEach((validation) => {
-    const validationMessage = validation.call(value, value)
-    if (validationMessage !== true) {
-      console.warn(`WARNING: Config parameter ${property} did not pass validation. ${validationMessage}`)
-      configWarnings += 1
-    }
-  })
-
-  return value
-}
-
-/**
- * Set an optional config environment variable. This setting will generate a warning only if it fails validation
- * @param {string} property The environment variable name
- * @param {[Function]} validations array of validation functions to run
- * @param {*} defaultValue default value to be provided for this, if any
- * @returns {*|undefined}
- */
-function optional (property, validations, defaultValue = undefined) {
-  const value = process.env[property] ?? defaultValue
-
-  validations.forEach((validation) => {
-    const validationMessage = validation.call(value, value)
-    if (validationMessage !== true) {
-      console.warn(`WARNING: Config parameter ${property} did not pass validation. ${validationMessage}`)
-      configWarnings += 1
-    }
-  })
-
-  return value
-}
 
 export default config
